9bb9bc187faf5f0e118511ff31c17190891445fa
gabrielcampanile <campanile@unifesp.br>
2025-05-24 17:17:31 -0300

    update kernel banners

--- minix/kernel/main.c
+++ minix/kernel/main.c
@@ -340,7 +340,10 @@ static void announce(void)
 #ifdef _VCS_REVISION
 	"(" _VCS_REVISION ")\n"
 #endif
-      "Copyright 2016, Vrije Universiteit, Amsterdam, The Netherlands\n",
+      "========================================\n
+      | Minix 3.4.0rc6 - SO - UNIFESP 1s2025 |\n
+      | Projeto Pratico (I) - Equipe 4 |\n
+      ========================================\n",
       OS_RELEASE);
   printf("MINIX is open source software, see http://www.minix3.org\n");
 }
@@ -357,7 +360,10 @@ void prepare_shutdown(const int how)
    * do shutdown work.  Set a watchog timer to call shutdown(). The timer 
    * argument passes the shutdown status. 
    */
-  printf("MINIX will now be shut down ...\n");
+  printf("=======================================\n
+        | ATE A PROXIMA - SO - UNIFESP 1s2025 |\n
+        | Projeto Pratico (I) - Equipe 4 |\n
+        =======================================\n");
   set_kernel_timer(&shutdown_timer, get_monotonic() + system_hz,
       minix_shutdown, how);
 }
